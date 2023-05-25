193f1f724a4e9b336d6230250444ee523092c2eb
Anakin Childerhose <65310735+anakin4747@users.noreply.github.com>
2023-05-25 02:48:00 -0400

    Delete BluetoothForLinuxDevelopers-V1_0_1/BluetoothForLinuxDevelopers - V1_0_1 directory

--- BluetoothForLinuxDevelopers-V1_0_1/BluetoothForLinuxDevelopers - V1_0_1/code/microbit/microbit.js
+++
@@ -1,8 +0,0 @@
-bluetooth.onBluetoothConnected(function () {
-    basic.showString("C")
-})
-bluetooth.onBluetoothDisconnected(function () {
-    basic.showString("D")
-})
-bluetooth.startTemperatureService()
-bluetooth.startLEDService()
